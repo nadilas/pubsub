f2b539ce9d9c3e0cbd37a7892bac9e783e18b211
Alex Barlow <a@alexb.cc>
2020-04-17 16:30:17 +0100

    Use project ID not topic id

--- providers/google/google.go
+++ providers/google/google.go
@@ -186,7 +186,7 @@ func (g *GoogleCloud) subscribe(opts ps.HandlerOptions, h ps.MsgHandler, ready c
 						PublishTime: &pt,
 						Ack: func() {
 							req := &pbpb.AcknowledgeRequest{
-								Subscription: "projects/" + opts.Topic + "/subscriptions/" + subName,
+								Subscription: "projects/" + g.projectID + "/subscriptions/" + subName,
 								AckIds:       []string{w.ackID},
 							}
 
